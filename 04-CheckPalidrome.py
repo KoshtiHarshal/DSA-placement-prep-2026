542fcab11281da391e1f06a874ccebd16b2cd257
Koshti Harshal <142722058+KoshtiHarshal@users.noreply.github.com>
2026-04-01 21:15:14 +0530

    Delete 04-CheckPalidrome.py

--- 04-CheckPalidrome.py
+++
@@ -1,9 +0,0 @@
-num = input("Enter a number: ")
-temp = num
-while temp>0:
-    digit = temp % 10
-    rev = rev * 10 + digit
-    temp //= 10
-if num == rev:
-    print(num, "is a palindrome.")  
-else:    print(num, "is not a palindrome.") 
