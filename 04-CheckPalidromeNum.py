b08a5da96ba81d3548073e501bff165750884f7a
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-05-22 15:54:20 +0530

    DSA D1

--- 04-CheckPalidromeNum.py
+++ 04-CheckPalidromeNum.py
@@ -1,10 +1,12 @@
 # Check if a number is a palindrome
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
 temp = num
+rev = 0
 while temp>0:
     digit = temp % 10
     rev = rev * 10 + digit
     temp //= 10
 if num == rev:
     print(num, "is a palindrome.")  
-else:    print(num, "is not a palindrome.") 
+else:   
+    print(num, "is not a palindrome.")
