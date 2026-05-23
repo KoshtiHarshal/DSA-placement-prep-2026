6b1c02b034b7fcf89eedce34fa28159e06544207
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-05-23 19:36:02 +0530

    DSA D2

--- 08-PowerOfTwo.py
+++ 08-PowerOfTwo.py
@@ -1,12 +1,15 @@
-def power_of_two(n):
-    if n<=0:
+def isPowerOfTwo(n: int) -> bool:
+    # Any number less than or equal to 0 cannot be a power of two
+    if n <= 0:
         return False
-    elif n==1:
-        return True
-    elif n%2!=0:
-        return False
-    else:   
-        return True
+        
+    # Keep dividing by 2 as long as the number is even
+    while n % 2 == 0:
+        n = n // 2
+        
+    # If we successfully reached 1, it was a power of two
+    return n == 1
 
-n= int(input("Enter a number: "))
-print(power_of_two(n))
+print(isPowerOfTwo(1))    # True
+print(isPowerOfTwo(2))    # True
+print(isPowerOfTwo(10))   # False
--- 09-PowerOfThree.py
+++ 09-PowerOfThree.py
@@ -1,12 +1,18 @@
-def power_of_three(n):
-    if n<=0:
+# Power of Three
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+def isPowerOfThree(n: int) -> bool:
+    # Any number less than or equal to 0 cannot be a power of three
+    if n <= 0:
         return False
-    elif n==1:
-        return True
-    elif n%3!=0:
-        return False
-    else:
-        return True
         
-n= int(input("Enter a number: "))
-print(power_of_three(n))
+    # Keep dividing by 3 as long as the number is divisible by 3
+    while n % 3 == 0:
+        n = n // 3
+        
+    # If we successfully reached 1, it was a power of three
+    return n == 1
+
+print(isPowerOfThree(27))  # True
+print(isPowerOfThree(81))  # True
+print(isPowerOfThree(10))  # False
