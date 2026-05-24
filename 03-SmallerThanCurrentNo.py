103bb918a3b7ea9d2f48b15799bf10fb5305a53d
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-05-24 21:39:37 +0530

    DSA D1

--- 03-SmallerThanCurrentNo.py
+++ 03-SmallerThanCurrentNo.py
@@ -1,3 +1,6 @@
+# How Many Numbers Are Smaller Than the Current Number
+# Given the array List1, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
 List1 = list(map(int, input("Enter elements: ").split(',')))
 List2 = []
 
